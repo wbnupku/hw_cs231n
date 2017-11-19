e801dd665fc2531f26a1d019511c12c4358e2b36
Wang Xiao <wbnupku@gmail.com>
2017-11-19 22:16:44 +0800

    add

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -45,6 +45,7 @@ def svm_loss_naive(W, X, y, reg):
     loss /= num_train
     dW /= num_train
     # AFTER THE BATCH
+
     dW += 2 * reg * W
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
@@ -83,30 +84,98 @@ def svm_loss_vectorized(W, X, y, reg):
     margins[range(0, num_train), y] = 0
     margins[margins < 0] = 0
     toc = time.time()
-    # print toc - tic
+    print toc - tic
     loss = np.sum(margins) / num_train + reg * np.sum(W * W)
     mask_mat = np.zeros(margins.shape)
     mask_mat[margins > 0] = 1
     toc = time.time()
-    # print 'mask_mat[margins > 0] = 1', toc - tic
+    print 'mask_mat[margins > 0] = 1', toc - tic
     cube = X[:, :, np.newaxis] * mask_mat[:, np.newaxis, :]
     toc = time.time()
-    # print 'cube =', toc - tic
+    print 'cube =', toc - tic
     correct_class_gradients = -1.0 * np.sum(cube, axis=2)
     toc = time.time()
-    # print 'correct_class_gradients =', toc - tic
+    print 'correct_class_gradients =', toc - tic
     cube[range(0, num_train), :, y] += correct_class_gradients
     toc = time.time()
-    # print 'cube[range(0, num_train), :, y]:', toc - tic
+    print 'cube[range(0, num_train), :, y]:', toc - tic
     dW = np.sum(cube, axis=0)
-    # print dW[range(0, num_train), y].shape
-    # print correct_class_gradients.shape
+    print dW[range(0, num_train), y].shape
+    print correct_class_gradients.shape
     toc = time.time()
-    # print 'dW = np.sum(cube, axis=0):', toc - tic
+    print 'dW = np.sum(cube, axis=0):', toc - tic
     dW /= num_train
     dW += 2 * reg * W
     toc = time.time()
-    # print toc - tic
+    print toc - tic
+    #############################################################################
+    #                             END OF YOUR CODE                              #
+    #############################################################################
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the gradient for the structured SVM     #
+    # loss, storing the result in dW.                                           #
+    #                                                                           #
+    # Hint: Instead of computing the gradient from scratch, it may be easier    #
+    # to reuse some of the intermediate values that you used to compute the     #
+    # loss.                                                                     #
+    #############################################################################
+    pass
+    #############################################################################
+    #                             END OF YOUR CODE                              #
+    #############################################################################
+
+    return loss, dW
+
+
+def svm_loss_vectorized_1(W, X, y, reg):
+    """
+    Structured SVM loss function, vectorized implementation.
+
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    loss = 0.0
+    dW = np.zeros(W.shape)  # initialize the gradient as zero
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the structured SVM loss, storing the    #
+    # result in loss.                                                           #
+    #############################################################################
+    pass
+    tic = time.time()
+    num_train = y.shape[0]
+    scores = X.dot(W)
+    correct_class_score = scores[range(0, num_train), y]
+    margins = (scores - correct_class_score[:, np.newaxis] + 1)
+    margins[range(0, num_train), y] = 0
+    margins[margins < 0] = 0
+    toc = time.time()
+    print toc - tic
+    loss = np.sum(margins) / num_train + reg * np.sum(W * W)
+    mask_mat = np.zeros(margins.shape)
+    mask_mat[margins > 0] = 1
+    toc = time.time()
+    print 'mask_mat[margins > 0] = 1', toc - tic
+    cube = X[:, :, np.newaxis] * mask_mat[:, np.newaxis, :]
+    toc = time.time()
+    print 'cube =', toc - tic
+    correct_class_gradients = -1.0 * np.sum(cube, axis=2)
+    toc = time.time()
+    print 'correct_class_gradients =', toc - tic
+    cube[range(0, num_train), :, y] += correct_class_gradients
+    toc = time.time()
+    print 'cube[range(0, num_train), :, y]:', toc - tic
+    dW = np.sum(cube, axis=0)
+    print dW[range(0, num_train), y].shape
+    print correct_class_gradients.shape
+    toc = time.time()
+    print 'dW = np.sum(cube, axis=0):', toc - tic
+    dW /= num_train
+    dW += 2 * reg * W
+    toc = time.time()
+    print toc - tic
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
